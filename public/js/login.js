8bc6689aff1bf0a7ed71af9bd6c71a585f209f0b
Shane Curtis <c84a@ymail.com>
2021-09-27 20:46:10 +0800

    Completed login and logout functionality

---
+++ public/js/login.js
@@ -0,0 +1,26 @@
+
+
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+  const username = document.querySelector('#username').value.trim().toUpperCase();
+  const password = document.querySelector('#password').value;
+
+  if (username && password) {
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Unable to login!");
+    }
+  }
+};
+
+document.querySelector('#log-in-btn').addEventListener('click', loginFormHandler);
